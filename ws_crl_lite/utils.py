79d42eee2f38f9626e3485cd4036068c4a47e4d5
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2023-11-28 18:19:28 -0500

    updated code structure

---
+++ ws_crl_lite/utils.py
@@ -0,0 +1,18 @@
+import nflows
+from nflows.transforms import Transform
+
+
+class ConditionalAffineScalarTransform(Transform):
+    def __init__(self, param_net):
+        super().__init__()
+        self.param_net = param_net
+
+    def forward(self, inputs, context=None):
+        scale, shift, logabsdet = self.param_net(context)
+        outputs = inputs * scale + shift
+        return outputs, logabsdet
+
+    def inverse(self, inputs, context=None):
+        scale, shift, logabsdet = self.param_net(context)
+        outputs = (inputs - shift) / scale
+        return outputs, -logabsdet
